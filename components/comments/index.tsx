74ab387c68622b65dffaf4434ef5556ec9707bc4
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2021-11-01 12:40:56 +0400

    change some function in comments

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -6,6 +6,7 @@ import {FaRegStickyNote} from 'react-icons/fa'
 import {FiFlag} from 'react-icons/fi'
 import {AiFillEyeInvisible, AiFillPicture} from 'react-icons/ai'
 import {GrAttachment} from 'react-icons/gr'
+import moment from 'moment';
 
 interface IComments {
     text: string,
@@ -28,7 +29,7 @@ const Comments: FC<IComments> = ({user, text, createdAt}) => {
         setReplay(false)
     }
     const handleReply = () => {
-        setReplay(true)
+        setReplay(!reply)
         setDots(false)
     }
 
@@ -37,13 +38,17 @@ const Comments: FC<IComments> = ({user, text, createdAt}) => {
         return () => window.removeEventListener('click', replyClear)
     }, [dots])
     const replyClear = (e: any) => {
-        if (!e.path.includes(replayRef.current)) setDots(false)
+        if (!e.path.includes(replayRef.current)) {
+            setDots(false)
+            setReplay(false)
+        }
     }
+
 
 
     return (
         <>
-            <div className='my-5 '>
+            <div ref={replayRef} className='my-5  '>
 
                 <div className='flex items-center space-x-5'>
                     <Image src={user.avatarUrl} width={40} height={40} alt='avatar' className='rounded-full'/>
@@ -53,7 +58,7 @@ const Comments: FC<IComments> = ({user, text, createdAt}) => {
                 <div>
                     <p className='text-gray-600'>{text}</p>
                 </div>
-                <div ref={replayRef} className='mt-3'>
+                <div  className='mt-3'>
                     <div className='flex items-center space-x-5'>
                         <p onClick={handleReply}
                            className='text-gray-500 text-xs font-poppins cursor-pointer hover:text-green-400'>Reply</p>
@@ -64,7 +69,7 @@ const Comments: FC<IComments> = ({user, text, createdAt}) => {
                     {reply && (
                         <div className=' w-full h-36 border border-gray-600   grid grid-rows-12  my-3'>
                             <div className='bg-red-100 row-span-4'>
-                                <textarea name="reply" className='px-1 w-full h-full scrollbar-hide outline-none resize-none'> </textarea>
+                                <textarea name="reply" className='px-1 w-full h-full scrollbar-hide outline-none resize-none'/>
                             </div>
                             <div className=' row-span-8 flex justify-between items-center px-5'>
                                 <div className='flex items-center space-x-5'>
